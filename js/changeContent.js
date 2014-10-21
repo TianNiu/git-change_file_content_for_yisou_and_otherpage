72c33f7a09ef52d372ad7f3948a9e2c3bf645e0b
SimBoo <thisiswoa@163.com>
2014-10-21 15:20:41 +0800

    node基本可运行文件结构
    包含express，可能暂时用不到，先放着

---
+++ js/changeContent.js
@@ -0,0 +1,12 @@
+/************************************************
+ * 
+ ************************************************/
+//文件模块
+var fs = require('fs-extra');
+var util = require('util');
+var cheerio = require('cheerio');
+
+exports.main=function(){
+    console.log("i am superman!");
+    
+};
